757783dd6ac5a3e7cef0910d8f81d7c152c76c54
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-07-31 08:36:49 +0200

    update security rules and enable anonymous auth for dev

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet} from '@angular/router';
 import {Header} from "./shared/components/header/header";
 import {Footer} from "./shared/components/footer/footer";
 import {SeedService} from "./core/seed.service";
+import {Auth, signInAnonymously} from "@angular/fire/auth";
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,13 @@ import {SeedService} from "./core/seed.service";
 export class App implements OnInit{
   protected title = 'TravelHub';
 
-  constructor(private seed: SeedService) {}
+  constructor(private seed: SeedService, private auth: Auth) {
+    signInAnonymously(this.auth).then(cred => console.log('anon uid= ', cred.user.uid))
+        .catch(err => console.error('Anon sign in failed ', err));
+  }
 
   ngOnInit(): void {
         this.seed.seedOnce();
     }
-
 
 }
