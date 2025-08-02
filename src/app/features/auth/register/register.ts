8aea41c9e95a331d227d1564aefd602e0ffa99af
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-08-02 15:07:52 +0200

    navigation links using routerLink. displayName and email validation in the registration form + dynamically displaying individual error messages based on the specific case

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Router, RouterLink} from "@angular/router";
 import {AuthService} from "../../../core/services/auth.service";
 
@@ -17,11 +17,53 @@ export class Register {
     constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) {
         this.registerForm = this.formBuilder.group(
             {
-                displayName: ['', Validators.required],
-                email: ['', Validators.required],
+                displayName: ['', [Validators.required, Validators.minLength(5)]],
+                email: ['', [Validators.required, Validators.pattern(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)]],
                 password: ['', Validators.required]
             }
         )
+    }
+
+    get displayName(): AbstractControl<any,any> | null  {
+        return this.registerForm.get('displayName');
+    }
+
+    get email(): AbstractControl<any,any> | null  {
+        return this.registerForm.get('email');
+    }
+
+    get password(): AbstractControl<any,any> | null  {
+        return this.registerForm.get('password');
+    }
+
+    get isDisplayNameNotValid() : boolean {
+        return this.displayName?.invalid && (this.displayName?.dirty || this.displayName?.touched) || false;
+    }
+
+    get isEmailNotValid() : boolean {
+        return this.email?.invalid && (this.email?.dirty || this.email?.touched) || false;
+    }
+
+    get displayNameErrorMessage(): string {
+        if (this.displayName?.errors?.['required']) {
+            return 'Display Name is required!';
+        }
+        if (this.displayName?.errors?.['minlength']) {
+            return 'Minimum of 5 characters!';
+        }
+        return '';
+    }
+
+    get emailErrorMessage(): string {
+        if(this.email?.errors?.['required']) {
+            return 'Email is required';
+        }
+
+        if(this.email?.errors?.['pattern']) {
+            return 'Email is not valid';
+        }
+
+        return '';
     }
 
     onSubmit() {
